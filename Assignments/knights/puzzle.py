2b96c329af371580f2b7f5ccd1837a2d7a5335b6
John Ling <johnlingbusiness@gmail.com>
2023-08-16 13:53:19 +0800

    Added minesweeper

--- Assignments/knights/puzzle.py
+++ Assignments/knights/puzzle.py
@@ -11,22 +11,50 @@ CKnave = Symbol("C is a Knave")
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
+# If the statement that one is a Knave and a Knight is actually true they must be a knight
+# But if the statement is false (which it is) then they must be a Knave
+
+# Expected answer: A is a knave
+
 knowledge0 = And(
-    # TODO
+    Not(And(AKnight, AKnave)),
+    Implication(And(AKnight, AKnave), AKnight), 
+    Implication(Not(And(AKnight, AKnave)), AKnave) 
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
-knowledge1 = And(
-    # TODO
-)
 
+# A cannot be a knight because if the statement "we are both knaves" is true then A would not be a knight despite being truthful
+# Therefore A is a knave
+
+# Since we are both knaves is false then B must be a knight since not BOTH of them are knaves but A is
+# Therefore B is a knight
+
+# Expected answer: A is a knight and B is a knight
+
+knowledge1 = And(
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    Implication(And(AKnave, BKnave), AKnight),
+    Implication(Not(And(AKnave, BKnave)), AKnave),
+    Implication(And(AKnave, BKnave), BKnave),
+    Implication(Not(And(AKnave, BKnave)), BKnight)
+)
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+
+# If A is a knight then B would be lying (B would also be a knight due to A's statement) which is not possible if B is a knight
+# If A was a knave then B would still be lying but it would be ok since B is a knave
+
+# Expected answer: A and B are knaves
+
 knowledge2 = And(
-    # TODO
+    Implication(AKnight, And(AKnight, BKnight)),
+    Implication(AKnave, Not(And(AKnight, BKnight)))
 )
 
 # Puzzle 3
---
+++ Assignments/minesweeper/test.py
@@ -0,0 +1,49 @@
+def main():
+    arr = [[1, 2, 3, 4, 5, 6, 7, 8],
+           [1, 2, 3, 4, 5, 6, 7, 8],
+           [1, 2, 3, 4, 5, 6, 7, 8],
+           [1, 2, 3, 4, 5, 6, 7, 8],
+           [1, 2, 3, 4, 5, 6, 7, 8],
+           [1, 2, 3, 4, 5, 6, 7, 8],
+           [1, 2, 3, 4, 5, 6, 7, 8],
+           [1, 2, 3, 4, 5, 6, 7, 8]]
+
+    print(get_neighbors((6, 6)))
+
+def get_neighbors(cell):
+    """
+    Attempts to extract a cell's neighbors within a 3x3 square
+    """
+    GRID_SIZE = 8
+
+    startCell = [cell[0] - 1, cell[1] - 1]
+    cells = set()
+
+    startCountI = 0 # How many rows are missing due to being out of bounds
+    startCountJ = 0 # How many colunmns are missing due to being out of bounds
+
+    # Check if left upper vertical cell is out of bounds and adjust if so
+    if cell[0] - 1 < 0: 
+        startCell[0] = cell[0]
+        startCountI += 1
+    if cell[1] - 1 < 0: 
+        startCell[1] = cell[1]
+        startCountJ += 1
+
+    indexI = startCell[0]
+    countI = startCountI
+    while indexI < GRID_SIZE and countI != 3:
+        countJ = startCountJ
+        indexJ = startCell[1]
+        while indexJ < GRID_SIZE and countJ != 3:
+            cells.add((indexI, indexJ))
+            countJ += 1
+            indexJ += 1
+
+        indexI += 1
+        countI += 1
+    
+    return cells
+
+if __name__ == "__main__":
+    main() 
